04afefea28de3298439c7940cc8a091f64ff74ff
kajal8512 <kajaly20@navgurukul.org>
2023-11-13 12:55:25 +0530

    initial todo

---
+++ src/componanets/TodoForm/TodoForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TodoForm() {
+    return (
+        <form>
+        <input type="text" />
+        <button type="submit">Add</button>
+        </form>
+    );
+    }
