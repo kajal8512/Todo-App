2da9b8d06dd1039d451e08224222638e91e6e220
kajal8512 <kajaly20@navgurukul.org>
2023-11-15 20:21:51 +0530

    add delete functionality

--- src/componanets/TodoForm/TodoForm.jsx
+++ src/componanets/TodoForm/TodoForm.jsx
@@ -9,10 +9,16 @@ export default function TodoForm() {
         setTodo(e.target.value);
     };
 
+    // add todo to todoList
     const handleAddTodo = () => {
         setTodoList((prevTodoList) => [...prevTodoList, todo]);
         setTodo("");
     };
+
+    // remove todo from todoList
+    const handleRemoveTodo = (todo) => {
+        setTodoList((prevTodoList) => prevTodoList.filter((item) => item !== todo));
+    }
 
     return (
         <div className={styles.maincontainer}>
@@ -26,10 +32,16 @@ export default function TodoForm() {
                 />
             <button type="submit" className={styles.add} onClick={handleAddTodo}>Add</button>
             <ol>
-                    {todoList.map((item) => (
-                        <li key={item}>{item}</li>
-                    ))}
-                </ol>
+                {todoList.map((item) => (
+                    <li key={item}>{item} <button
+                    type="button"
+                    className={styles.remove}
+                    onClick={() => handleRemoveTodo(item)}
+                    >Remove</button>
+                </li>
+                ))}
+            </ol>
+           
         </div>
     );
 }
