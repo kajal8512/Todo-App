6f39156cced5cb0c4bdf8d10844897dfff395310
kajal8512 <kajaly20@navgurukul.org>
2023-11-15 19:33:22 +0530

    add functions

--- src/componanets/TodoForm/TodoForm.jsx
+++ src/componanets/TodoForm/TodoForm.jsx
@@ -2,13 +2,34 @@ import React from "react";
 import styles from "./TodoForm.module.css";
 
 export default function TodoForm() {
+    const [todo, setTodo] = React.useState('');
+    const [todoList, setTodoList] = React.useState([]);
+
+    const handleTodoChange = (e) => {
+        setTodo(e.target.value);
+    };
+
+    const handleAddTodo = () => {
+        setTodoList((prevTodoList) => [...prevTodoList, todo]);
+        setTodo("");
+    };
+
     return (
         <div className={styles.maincontainer}>
-        <div className={styles.container}>
             <label htmlFor="todo" className={styles.heading}>Todo App</label>
-            <input id="todo" className={styles.addInput} type="text" />
-            <button type="submit" className={styles.add}>Add</button>
-        </div>
+            <input
+                    id="todo"
+                    className={styles.addInput}
+                    type="text"
+                    value={todo}
+                    onChange={handleTodoChange}
+                />
+            <button type="submit" className={styles.add} onClick={handleAddTodo}>Add</button>
+            <ol>
+                    {todoList.map((item) => (
+                        <li key={item}>{item}</li>
+                    ))}
+                </ol>
         </div>
     );
 }
