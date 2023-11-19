5b9e292dd5f13c9128401061934142303104c534
kajal8512 <kajaly20@navgurukul.org>
2023-11-19 08:56:10 +0530

    add edit button

--- src/componanets/TodoForm/TodoForm.jsx
+++ src/componanets/TodoForm/TodoForm.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./TodoForm.module.css";
 
 export default function TodoForm() {
-    const [todo, setTodo] = React.useState('');
-    const [todoList, setTodoList] = React.useState([]);
+    const [todo, setTodo] = useState('');
+    const [todoList, setTodoList] = useState([]);
+    const [editTodo, setEditTodo] = useState(true);
 
     const handleTodoChange = (e) => {
         setTodo(e.target.value);
@@ -11,13 +12,29 @@ export default function TodoForm() {
 
     // add todo to todoList
     const handleAddTodo = () => {
-        setTodoList((prevTodoList) => [...prevTodoList, todo]);
+        if (editTodo !== null) {
+            // Edit existing todo
+            const updatedList = todoList.map((item) =>
+                item === editTodo ? todo : item
+            );
+            setTodoList(updatedList);
+            setEditTodo(null);
+        } else {
+            // Add new todo
+            setTodoList((prevTodoList) => [...prevTodoList, todo]);
+        }
         setTodo("");
     };
 
     // remove todo from todoList
     const handleRemoveTodo = (todo) => {
         setTodoList((prevTodoList) => prevTodoList.filter((item) => item !== todo));
+    }
+
+    // edit todo from todoList
+    const handleEditTodo = (todo) => {
+        setEditTodo(todo);
+        setTodo(todo);
     }
 
     return (
@@ -30,14 +47,27 @@ export default function TodoForm() {
                     value={todo}
                     onChange={handleTodoChange}
                 />
-            <button type="submit" className={styles.add} onClick={handleAddTodo}>Add</button>
+                 <button type="submit" className={styles.add} onClick={handleAddTodo}>
+                {editTodo !== null ? "Edit" : "Add"}
+                </button>
             <ol>
                 {todoList.map((item) => (
-                    <li key={item}>{item} <button
-                    type="button"
-                    className={styles.remove}
-                    onClick={() => handleRemoveTodo(item)}
-                    >Remove</button>
+                     <li key={item}>
+                     {item}{" "}
+                     <button
+                         type="button"
+                         className={styles.remove}
+                         onClick={() => handleEditTodo(item)}
+                     >
+                         Edit
+                     </button>{" "}
+                     <button
+                         type="button"
+                         className={styles.remove}
+                         onClick={() => handleRemoveTodo(item)}
+                     >
+                         Delete
+                     </button>
                 </li>
                 ))}
             </ol>
